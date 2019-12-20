ac3cc678d7c40ab69e5768cb6d3605f486946193
Catherine <yacatherine22@gmail.com>
2019-12-20 17:54:22 +0800

    added eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+    root: true,
+    env: {
+        es6: true
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            quotes: ['error', 'single'],
+            indent: ['error', 4],
+            semi: [1, 'always']
+        }
+    },
+    parser: 'babel-eslint',
+    extends: ['@react-native-community', 'prettier', 'prettier/react']
 };
